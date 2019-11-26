996fbfb7eeff54714d0dd7c3016575e95b0fe4aa
harkeshkumar3 <harkesh.kumar@bellurbis.com>
2019-11-26 12:08:03 +0530

    projet setup

---
+++ src/test/java/com/packer/Packer/PackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packer.Packer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
